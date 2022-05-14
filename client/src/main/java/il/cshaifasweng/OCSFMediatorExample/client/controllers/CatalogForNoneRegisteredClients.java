464674e37976a04a17f105cdc91e97bfc658ac13
mostafa egbarea <mostafaigbarea@gmail.com>
2022-05-14 12:11:32 +0300

    added initial controllers for client

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/CatalogForNoneRegisteredClients.java
@@ -0,0 +1,19 @@
+package il.cshaifasweng.OCSFMediatorExample.client.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class CatalogForNoneRegisteredClients {
+
+    @FXML // fx:id="backBtn"
+    private Button backBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="itemsContainer"
+    private AnchorPane itemsContainer; // Value injected by FXMLLoader
+
+    @FXML // fx:id="title"
+    private TextField title; // Value injected by FXMLLoader
+
+}
---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/ClientMainPage.java
@@ -0,0 +1,20 @@
+package il.cshaifasweng.OCSFMediatorExample.client.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ClientMainPage {
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private Button cartBtn;
+
+    @FXML
+    private Button catalogBtn;
+
+    @FXML
+    private Button customizedOrderBtn;
+
+}
