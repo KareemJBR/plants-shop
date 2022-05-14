9584d557bd6ec56f3f471c83bd4abaa686c0cebd
Kareem <77075186+KareemJBR@users.noreply.github.com>
2022-05-14 07:38:56 +0300

    Built interface for admin main windows with basic functionality

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/ShowAllCustomers.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/ShowAllCustomers.java
@@ -1,11 +1,21 @@
 package il.cshaifasweng.OCSFMediatorExample.client.controllers;
 
+import il.cshaifasweng.OCSFMediatorExample.client.App;
+import il.cshaifasweng.OCSFMediatorExample.entities.Customer;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
+
+import java.io.IOException;
 
 public class ShowAllCustomers {
 
     @FXML
-    private TableView<?> customersTable;
+    private TableView<Customer> customersTable;
+
+    @FXML
+    void backToHomePage(ActionEvent event) throws IOException {
+        App.setRoot("AdministratorHomePage");
+    }
 
 }
--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/ShowReportsForAdmin.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/ShowReportsForAdmin.java
@@ -1,4 +1,20 @@
 package il.cshaifasweng.OCSFMediatorExample.client.controllers;
 
+import il.cshaifasweng.OCSFMediatorExample.client.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+import java.io.IOException;
+
 public class ShowReportsForAdmin {
+
+    @FXML
+    private TableView<?> reportsTable;
+
+    @FXML
+    void backToHomePage(ActionEvent event) throws IOException {
+        App.setRoot("AdministratorHomePage");
+    }
+
 }
