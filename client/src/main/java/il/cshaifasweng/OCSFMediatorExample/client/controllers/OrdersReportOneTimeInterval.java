ba11bf1df03cc1aa5ef80e3910ebca4351cfc10c
Kareem <77075186+KareemJBR@users.noreply.github.com>
2022-05-24 13:25:04 +0300

    Created the interface for Admin reports

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/OrdersReportOneTimeInterval.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/OrdersReportOneTimeInterval.java
@@ -1,4 +1,24 @@
 package il.cshaifasweng.OCSFMediatorExample.client.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+
+import java.util.Date;
+
 public class OrdersReportOneTimeInterval {
+
+    @FXML
+    private CategoryAxis dayAxes;
+
+    @FXML
+    private NumberAxis ordersNumAxes;
+
+    @FXML
+    private BarChart<String, Integer> reportsChart;
+
+    public void start_chart(int shop_id, Date start_date, Date end_date){
+        // TODO: initialize the chart
+    }
 }
