a7fda9692618cdec17755e045b8b4675176a4d09
Kareem <77075186+KareemJBR@users.noreply.github.com>
2022-05-24 13:55:46 +0300

    Adding controller for showing and editing customers' details

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/CustomerDetailsToEdit.java
@@ -0,0 +1,54 @@
+package il.cshaifasweng.OCSFMediatorExample.client.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class CustomerDetailsToEdit {
+
+    @FXML
+    private TextField customerAccountTypeTextField;
+
+    @FXML
+    private TextField customerEmailTextField;
+
+    @FXML
+    private TextField customerFirstNameTextField;
+
+    @FXML
+    private TextField customerIDTextField;
+
+    @FXML
+    private TextField customerLastNameTextField;
+
+    @FXML
+    private TextField customerPasswordTextField;
+
+    @FXML
+    private TextField customerUsernameTextField;
+
+    @FXML
+    void backToCustomersView(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteCustomer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void resetButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveChanges(ActionEvent event) {
+
+    }
+
+    public void start_controller(String customer_id){
+
+    }
+
+}
