01134eb94ccfd4cefb4d021de66ce7ce9ad8f7c9
Kareem <77075186+KareemJBR@users.noreply.github.com>
2022-05-26 02:14:49 +0300

    Create NetWorkerHomePage.java

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/NetWorkerHomePage.java
@@ -0,0 +1,21 @@
+package il.cshaifasweng.OCSFMediatorExample.client.controllers;
+
+import il.cshaifasweng.OCSFMediatorExample.client.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class NetWorkerHomePage {
+
+    @FXML
+    void openCatalogue(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signOut(ActionEvent event) throws IOException {
+        App.setRoot("controllers/LogIN");
+    }
+
+}
