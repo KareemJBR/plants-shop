f38a6f3f69f01c58fc4faae5a2dd34145796849c
Kareem <77075186+KareemJBR@users.noreply.github.com>
2022-05-26 02:48:37 +0300

    Built controllers for the last type of reports

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/OrdersReportOneTimeInterval.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/OrdersReportOneTimeInterval.java
@@ -1,4 +1,19 @@
 package il.cshaifasweng.OCSFMediatorExample.client.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+
 public class OrdersReportOneTimeInterval {
+
+    @FXML
+    private CategoryAxis itemAxes;
+
+    @FXML
+    private BarChart<?, ?> ordersChart;
+
+    @FXML
+    private NumberAxis ordersNumAxes;
+
 }
