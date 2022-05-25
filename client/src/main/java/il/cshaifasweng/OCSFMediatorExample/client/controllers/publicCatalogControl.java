09a4aa602146a737c94f0592e4c7b83809e18769
sameerNajjar <59772699+sameerNajjar@users.noreply.github.com>
2022-05-25 23:31:47 +0300

    add real dynamic catalog for public

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/publicCatalogControl.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/publicCatalogControl.java
@@ -7,17 +7,13 @@ import il.cshaifasweng.OCSFMediatorExample.entities.Item;
 import il.cshaifasweng.OCSFMediatorExample.entities.MsgClass;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TableColumn;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 
-
+import static il.cshaifasweng.OCSFMediatorExample.client.SimpleClient.Itemsdata;
 import static il.cshaifasweng.OCSFMediatorExample.client.controllers.SignUp.shop;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -30,15 +26,22 @@ public class publicCatalogControl {
     private Button backBtn;
 
     @FXML
-    private Button backBtn1;
+    private Button singUp;
 
-    @FXML
-    private TextField title;
+
 
     @FXML
     private TableView table=new TableView<Item>();
 
+    @FXML
+    private AnchorPane itemscontainer;
 
+    @FXML
+    private Label title;
+
+
+    @FXML
+    private AnchorPane itemsContainer;
     @FXML
     void Back(ActionEvent event) throws IOException {
         MsgClass msg=new MsgClass("#get customers",null);
@@ -55,24 +58,80 @@ public class publicCatalogControl {
         //SignUp.main(null);
     }
     @FXML
-    void initialize() {
-        try {
-            System.out.println("in init for catalog");
-            // ArrayList<Flower> flowers = getAllFlowers();
-            System.out.println(((ArrayList<Flower>)data));
-            /*text1.setText(((ArrayList<Flower>)data).get(0).toString());
-            img1.setImage(new Image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzjZZ9dDZzh6zb3fbq8g4MpK8ybBNNQ9TzEg&usqp=CAU"));
-            text2.setText(((ArrayList<Flower>)data).get(1).toString());
-            img2.setImage(new Image(((ArrayList<Flower>)data).get(1).getImgURL()));
-            text3.setText(((ArrayList<Flower>)data).get(2).toString());
-            img3.setImage(new Image(((ArrayList<Flower>)data).get(2).getImgURL()));
-            text4.setText(((ArrayList<Flower>)data).get(3).toString());
-            img4.setImage(new Image(((ArrayList<Flower>)data).get(3).getImgURL()));
-            text5.setText(((ArrayList<Flower>)data).get(4).toString());
-            img5.setImage(new Image(((ArrayList<Flower>)data).get(4).getImgURL()));*/
-        } catch (Exception e) {
-            System.out.println("init exception for reg catalog");
-            System.out.println(e);
+    public void initialize() throws IOException, InterruptedException {
+
+        ArrayList<Item> cartItems = (ArrayList<Item>) Itemsdata;
+        itemscontainer.setStyle("-fx-background-color: #222831");
+        backBtn.setStyle("-fx-background-color:#EEEEEE");
+        singUp.setStyle("-fx-background-color:#EEEEEE");
+        title.setStyle("-fx-background-color:#00ADB5");
+        itemsContainer.setStyle("-fx-background-color:#222831");
+        boolean moveRight = false;
+        int j = 0;
+        if (cartItems != null) {
+            if (cartItems.size() != 0) {
+                itemscontainer.setMinHeight(cartItems.size() * 80);      ///the height of the container is related to the amount of the items
+                ArrayList<ImageView> arr = new ArrayList<ImageView>();
+                for (int i = 0; i < cartItems.size(); i++) {
+                    AnchorPane p = new AnchorPane();            //container of each item
+                    p.setStyle("-fx-background-color: #393E46");
+                    p.setMinSize(295, 140);
+                    if (i % 2 == 1) {
+                        moveRight = true;
+                    } else {
+                        moveRight = false;
+                    }
+                    ////////////// img /////////////
+                    ImageView imageview = new ImageView();
+                    imageview.setFitWidth(130);   //width of img
+                    imageview.setFitHeight(130); //height of img
+                    System.out.println(i);
+                    imageview.setImage(new Image(cartItems.get(i).getUrl()));
+                    imageview.setLayoutX(5);           //x & y coordinate related in the pane
+                    imageview.setLayoutY(5);
+
+                    //////////////// details of the item //////////////
+                    ///////// price textfield ///////////
+                    TextField name = new TextField("Name: " + cartItems.get(i).getName());
+                    name.setStyle("-fx-background-color:#00ADB5");
+                    name.setLayoutX(140);
+                    name.setLayoutY(5);
+
+                    ///////// type catalog number ///////////
+                    TextField catologNum = new TextField("Catalog Number: " + cartItems.get(i).getCatalogNumber());
+                    catologNum.setStyle("-fx-background-color:#00ADB5");
+                    catologNum.setLayoutX(140);
+                    catologNum.setLayoutY(25);
+
+                    ///////// type textfield ///////////
+                    TextField type = new TextField("type: " + cartItems.get(i).getType());
+                    type.setStyle("-fx-background-color:#00ADB5");
+                    type.setLayoutX(140);           //x & y coordinate related in the pane
+                    type.setLayoutY(45);
+
+                    ///////// type textfield ///////////
+                    TextField price = new TextField("type: " + cartItems.get(i).getPrice());
+                    price.setStyle("-fx-background-color:#00ADB5");
+                    price.setLayoutX(140);           //x & y coordinate related in the pane
+                    price.setLayoutY(65);
+
+                    
+                    /////////////// adding components to the pane /////////////
+                    p.getChildren().add(imageview);
+                    p.getChildren().add(price);
+                    p.getChildren().add(type);
+                    p.getChildren().add(name);
+                    p.getChildren().add(catologNum);
+                    if (moveRight) {
+                        p.setLayoutY(170 * j);
+                        p.setLayoutX(320);
+                        j++;
+                    } else {
+                        p.setLayoutY(170 * j);
+                    }
+                    itemscontainer.getChildren().add(p);
+                }
+            }
         }
     }
 
