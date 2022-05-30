ee5a0428f38a0e9c1eb82b6002746fe3a6523adf
Kareem <77075186+KareemJBR@users.noreply.github.com>
2022-05-30 06:50:23 +0300

    Update ShowReportsForClientService.java

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/ShowReportsForClientService.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/controllers/ShowReportsForClientService.java
@@ -11,7 +11,6 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
